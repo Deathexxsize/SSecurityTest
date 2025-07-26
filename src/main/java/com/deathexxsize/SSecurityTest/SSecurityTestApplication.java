d4b515697954acb7d7bf633ca3d38c2d36ff65a0
Deathexxsize <nurjigit006abdyganyev@gmail.com>
2025-07-26 18:15:31 +0600

    Learn security - part 1

---
+++ src/main/java/com/deathexxsize/SSecurityTest/SSecurityTestApplication.java
@@ -0,0 +1,14 @@
+package com.deathexxsize.SSecurityTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SSecurityTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SSecurityTestApplication.class, args);
+		System.out.println("GOOD");
+	}
+
+}
